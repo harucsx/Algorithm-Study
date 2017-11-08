1f61cd57578b0e87e8b6affd096ac3a9e88df8c8
nj0034 <nj0034@naver.com>
2017-11-08 13:35:15 +0900

    Solve 10844 Modify 11057

--- baekjoon/11057_nj0034.cpp
+++ baekjoon/11057_nj0034.cpp
@@ -5,27 +5,26 @@
 using namespace std;
 
 int N;
-long long int stair[101][11];
+long long int stair[1001][11];
 
 int main() {
     freopen("input.txt", "r", stdin);
     cin >> N;
-    stair[1][10] = 9;
-    stair[1][0] = 0;
-    for (int i = 1; i < 10; ++i) {
+    stair[1][10] = 10;
+    for (int i = 0; i < 10; ++i) {
         stair[1][i] = 1;
     }
     for (int i = 2; i <= N; ++i) {
-        stair[i][0] = stair[i - 1][1] % 1000000000;
-        stair[i][9] = stair[i - 1][8] % 1000000000;
-        for (int j = 1; j < 9; ++j) {
-            stair[i][j] = stair[i - 1][j - 1] % 1000000000 + stair[i - 1][j + 1] % 1000000000;
+        for (int j = 0; j < 10; ++j) {
+            for (int k = 0; k < j + 1; ++k) {
+                stair[i][j] += stair[i - 1][k] % 10007;
+            }
         }
         for (int k = 0; k < 10; ++k) {
-            stair[i][10] += stair[i][k] % 1000000000;
+            stair[i][10] += stair[i][k] % 10007;
         }
     }
-    cout << stair[N][10] % 1000000000;
+    cout << stair[N][10] % 10007;
     return 0;
 }
 
